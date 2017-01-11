02ae462478bef5ce68f6d5be9cf0156b59c136e6
andela-nrotimi <rotimi.nelson@andela.com>
2017-01-11 19:08:05 +0100

    chore(configuration): trim long lines of code

--- karma.conf.js
+++ karma.conf.js
@@ -55,12 +55,16 @@ module.exports = function(config) {
     logLevel: config.LOG_INFO,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
+    /* enable / disable watching file and executing tests 
+    ** whenever any file changes
+    */
     autoWatch: true,
 
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    /* start these browsers
+    ** available browser launchers: https://npmjs.org/
+    ** browse/keyword/karma-launcher
+    */
     browsers: ['Chrome', 'Firefox', 'PhantomJS'],
 
 
