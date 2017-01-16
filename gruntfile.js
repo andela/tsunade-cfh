0f37135107b3f0361449457ea6d08d46237efc6c
OlufemiOyewole <femioyewole03@gmail.com>
2017-01-16 13:09:53 +0100

    refactor(database): add default port

--- gruntfile.js
+++ gruntfile.js
@@ -47,9 +47,6 @@ module.exports = function(grunt) {
                     watchedFolders: ['app', 'config'],
                     debug: true,
                     delayTime: 1,
-                    // env: {
-                    //     PORT: 3000
-                    // },
                     cwd: __dirname
                 }
             }
