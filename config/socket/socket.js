1f38686f18c23a248bad629afb1623f15e447b7e
OlufemiOyewole <femioyewole03@gmail.com>
2017-01-18 00:51:08 +0100

    feature(player-number): set 3 player minimum when user wants to start the game and there are less than 3 people, a popup shows up explaining a minimum of 3 players are required to play the game

--- config/socket/socket.js
+++ config/socket/socket.js
@@ -61,6 +61,8 @@ module.exports = function(io) {
           });
           thisGame.prepareGame();
           thisGame.sendNotification('The game has begun!');
+        } else {
+          thisGame.sendNotification('Minimum of 3 players are required to play the game');
         }
       }
     });
