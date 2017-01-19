659d5b243a42ea778d94cb5a32b690778e22971f
andela-osubair <oyindamola.subair@andela.com>
2017-01-19 17:57:14 +0100

    chore(coveralls): update indentation error

--- gulpfile.js
+++ gulpfile.js
@@ -11,79 +11,79 @@ const istanbul = require('gulp-istanbul');
 
 
 gulp.task('watch', () => { // Watch tasks
-    gulp.watch(['public/css/common.scss',
-        'public/css/views/articles.scss'
-    ], ['sass']);
-    gulp.watch('app/views/**').on('change', browserSync.reload);
-    gulp.watch(['public/js/**', 'app/**/*.js'], ['jshint'])
-        .on('change', browserSync.reload);
-    gulp.watch('public/views/**').on('change', browserSync.reload);
-    gulp.watch('public/css/**', ['sass'])
-        .on('change', browserSync.reload);
-    gulp.watch('app/views/**', ['jade'])
-        .on('change', browserSync.reload);
+  gulp.watch(['public/css/common.scss',
+    'public/css/views/articles.scss'
+  ], ['sass']);
+  gulp.watch('app/views/**').on('change', browserSync.reload);
+  gulp.watch(['public/js/**', 'app/**/*.js'], ['jshint'])
+    .on('change', browserSync.reload);
+  gulp.watch('public/views/**').on('change', browserSync.reload);
+  gulp.watch('public/css/**', ['sass'])
+    .on('change', browserSync.reload);
+  gulp.watch('app/views/**', ['jade'])
+    .on('change', browserSync.reload);
 });
 
 gulp.task('jshint', () => gulp.src([
-        'gulpfile.js',
-        'app/**/*.js',
-        'test/**/*.js',
-        'public/js/**/*.js'
-    ])
-    .pipe(jshint()).pipe(jshint.reporter('jshint-stylish')));
+    'gulpfile.js',
+    'app/**/*.js',
+    'test/**/*.js',
+    'public/js/**/*.js'
+  ])
+  .pipe(jshint()).pipe(jshint.reporter('jshint-stylish')));
 
 gulp.task('lint', () => gulp.src([
-        'gulpfile.js',
-        'app/**/*.js',
-        'test/**/*.js',
-        'public/js/**/*.js'
-    ])
-    .pipe(eslint()));
+    'gulpfile.js',
+    'app/**/*.js',
+    'test/**/*.js',
+    'public/js/**/*.js'
+  ])
+  .pipe(eslint()));
 
 gulp.task('nodemon', () => {
-    nodemon({
-        script: 'server.js',
-        ext: 'js',
-        env: { NODE_ENV: 'development' }
-    });
+  nodemon({
+    script: 'server.js',
+    ext: 'js',
+    env: { NODE_ENV: 'development' }
+  });
 });
 
 gulp.task('server', ['nodemon'], () => {
-    browserSync.create({
-        proxy: 'localhost:3001',
-        server: 'server.js',
-        port: 3000,
-        reloadOnRestart: true
-    });
+  browserSync.create({
+    proxy: 'localhost:3001',
+    server: 'server.js',
+    port: 3000,
+    reloadOnRestart: true
+  });
 });
 
 gulp.task('pre-test', () => gulp.src(['test/**/*.js'])
-    .pipe(istanbul({ includeUntested: true }))
-    .pipe(istanbul.hookRequire()));
+  .pipe(istanbul({ includeUntested: true }))
+  .pipe(istanbul.hookRequire()));
 
 gulp.task('mochaTest', ['pre-test'], () => gulp.src(['./test/**/*.js'], {
-        read: false
-    })
-    .pipe(mocha({ reporter: 'spec' }))
-    .pipe(istanbul.writeReports({
-        dir: './coverage',
-        reporters: ['lcov'],
-        reportOpts: { dir: './coverage' },
-    }))
-    .once('error', () => {
-        process.exit(1);
-    })
-    .once('end', () => {
-        process.exit();
-    }));
+    read: false
+  })
+  .pipe(mocha({ reporter: 'spec' }))
+  .pipe(istanbul.writeReports({
+    dir: './coverage',
+    reporters: ['lcov'],
+    reportOpts: { dir: './coverage' },
+  }))
+  .once('error', () => {
+    process.exit(1);
+  })
+  .once('end', () => {
+    process.exit();
+  }));
 
 gulp.task('sass', () => gulp.src('public/css/common.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('public/css/')));
+  .pipe(sass())
+  .pipe(gulp.dest('public/css/')));
 
 gulp.task('bower', () => {
-    bower()
-        .pipe(gulp.dest('./public/lib'));
+  bower()
+    .pipe(gulp.dest('./public/lib'));
 });
 
 
