5fc01dff6c02ab9394fd7428e252974214e93d64
andela-osubair <oyindamola.subair@andela.com>
2017-01-19 15:49:46 +0100

    chore(coveralls): update indentation

--- test/user/model.js
+++ test/user/model.js
@@ -2,9 +2,9 @@
  * Module dependencies.
  */
 const should = require('should'),
-    app = require('../../server'),
-    mongoose = require('mongoose'),
-    User = mongoose.model('User');
+  app = require('../../server'),
+  mongoose = require('mongoose'),
+  User = mongoose.model('User');
 
 
 // Globals
@@ -12,35 +12,35 @@ let user;
 
 // The tests
 describe('<Unit Test>', () => {
-    describe('Model User:', () => {
-        before((done) => {
-            user = new User({
-                name: 'Full name',
-                email: 'test@test.com',
-                username: 'user',
-                password: 'password'
-            });
+  describe('Model User:', () => {
+    before((done) => {
+      user = new User({
+        name: 'Full name',
+        email: 'test@test.com',
+        username: 'user',
+        password: 'password'
+      });
 
-            done();
-        });
+      done();
+    });
 
-        describe('Method Save', () => {
-            it('should be able to save whithout problems',
-                () => user.save((err) => {
-                    should.not.exist(err);
-                }));
+    describe('Method Save', () => {
+      it('should be able to save whithout problems',
+        () => user.save((err) => {
+          should.not.exist(err);
+        }));
 
-            it('should be able to show an error when try to save witout name',
-                () => {
-                    user.name = '';
-                    return user.save((err) => {
-                        should.exist(err);
-                    });
-                });
-        });
-
-        after((done) => {
-            done();
+      it('should be able to show an error when try to save witout name',
+        () => {
+          user.name = '';
+          return user.save((err) => {
+            should.exist(err);
+          });
         });
     });
+
+    after((done) => {
+      done();
+    });
+  });
 });
