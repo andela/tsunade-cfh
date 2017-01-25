f0e0391c20afae5b46d3a2a6acb83f92e9e347fc
andela-nrotimi <rotimi.nelson@andela.com>
2017-01-25 11:31:24 +0100

    Make changes to landing page layout

--- public/js/init.js
+++ public/js/init.js
@@ -8,8 +8,34 @@
 if (window.location.hash == "#_=_") window.location.hash = "#!";
 // $(document).ready(function() {
 //     //Fixing facebook bug with redirect
-    
+
 
 //     //Then init the app
 //     window.init();
 // });
+
+$(document).ready(function () {
+      $('[data-toggle="tooltip"]').tooltip();
+      $('.carousel').carousel();
+      // Next slide
+      $('.carousel').carousel('next');
+      $('.carousel').carousel('next', 3); // Move next n times.
+      // Previous slide
+      $('.carousel').carousel('prev');
+      $('.carousel').carousel('prev', 4); // Move prev n times.
+      $('[data-toggle="tooltip"]').tooltip();
+      $("#toggleSwitch_j").hover(
+
+      function() {
+            $("#theBox_3").slideDown(500);
+      }, function() {
+            $("#theBox_3").slideUp(500);
+      });
+
+      $("#StayOpen").hover(
+      function() {
+            $("#theBox_2").slideDown(500);
+      }, function() {
+            $("#theBox_2").slideUp(500);
+      });
+});
