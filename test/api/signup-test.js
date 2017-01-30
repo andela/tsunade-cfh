4498b46626a3af809dbc9a4f6cdfd5c4ea1734b6
andela-uenyingwa <uloaku.enyingwa@andela.com>
2017-01-30 11:09:38 +0100

    feature(authentication): modify test file

--- test/api/signup-test.js
+++ test/api/signup-test.js
@@ -7,7 +7,7 @@ const chai_http = require('chai-http');
 const server = require('./../../server');
 const signup = require('./../../app/controllers/signup');
 
-const expect = chai.expect();
+const expect = chai.expect;
 
 chai.use(chai_http);
 
@@ -24,46 +24,79 @@ describe('Signup authentication', () => {
       done();
     });
   });
+  afterEach((done) => {
+    User.remove({}, (err) => {
+      if (err) {
+        throw new Error('Failed to wipe database');
+      }
+      done();
+    });
+  });
 
-  describe('Signup', () => {
-    it('should check if any input field is empty', (done) => {
-      const user = {
-        name: 'uloaku',
-        username: 'ulo',
-        email: '',
-        password: ''
-      };
+  it('should check if any input field is empty', (done) => {
+    const user = {
+      name: 'uloaku',
+      username: 'ulo',
+      email: '',
+      password: ''
+    };
 
-      chai.request(server)
-        .post('/controllers/signup')
-        .send(user)
-        .end((err, res) => {
-          expect(res.body).to.have.statusOf(400);
-          expect(res.body).to.have.property('msg');
-          expect(res.body.msg).to.equal('Please no input field can be empty!');
-          done();
-        });
+    chai.request(server)
+    .post('/api/auth/signup')
+    .send(user)
+    .end((err, res) => {
+      expect(res).to.have.status(400);
+      expect(res.body).to.have.property('msg');
+      expect(res.body.msg).to.equal('Please no input field can be empty!');
+      done();
     });
   });
 
 
-  it('should return error when a username or email already exists', () => {
+  it('should return error when a username or email already exists', (done) => {
+    // const user = new User();
+    // user.name = 'uloaku';
+    // user.username = 'ulo';
+    // user.password = 'password';
+    // user.email = 'ulo@gmail.com';
+    // user.save(() => {
+    //   done();
+    // });
     const user = {
       name: 'chioma',
-      username: 'ulo',
+      username: 'uloooo',
       email: 'ulo@gmail.com',
       password: 'chioma001'
     };
 
     chai.request(server)
-      .post('/controllers/signup')
+      .post('/api/auth/signup')
       .send(user)
       .end((err, res) => {
-        expect(res.body).to.have.statusOf(409);
+        expect(res).to.have.status(400);
         expect(res.body).to.have.property('msg');
-        expect(res.body.msg).to.equal('User already exists!');
+        expect(res.body.msg).to.equal('email already exist');
         done();
       });
+  });
+
+  it('should give a user JWT token on successful signup', (done) => {
+    user = {
+      name: 'uloaku',
+      username: 'ulojbkbk',
+      email: 'ulojjbjmbb@gmail.com',
+      password: 'password'
+    };
+    chai.request(server)
+    .post('/api/auth/signup')
+    .send(user)
+    .end((err, res) => {
+      expect(res).to.have.status(201);
+      expect(res.body).to.have.property('msg');
+      expect(res.body).to.have.property('token');
+      expect(res.body.msg).to.equal('You have successfully signed up!');
+      done();
+    });
   });
 });
 
