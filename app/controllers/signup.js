42101472a5c30da1d3a419667f08958e7b534b1d
andela-uenyingwa <uloaku.enyingwa@andela.com>
2017-01-30 11:07:17 +0100

    feature(authentication): modify JS file

--- app/controllers/signup.js
+++ app/controllers/signup.js
@@ -5,27 +5,34 @@ const moment = require('moment');
 const User = mongoose.model('User');
 
 exports.signup = (req, res) => {
-  if (!req.body.name || !req.body.email || !req.body.password) {
-    res.json({ success: false, msg: 'Please enter name, email and password.' });
-  } else {
-    const newUser = new User({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password
-    });
-    // save the user
-    newUser.save((err, user) => {
-      if (err) {
-        res.send(err);
-      } else {
-        const token = jwt.sign({
-          id: user.id,
-          exp: moment().add(7, 'd').valueOf()
-        }, 'secret');
-        res.json({
-          success: true, msg: 'Created new user successfully.', token
-        });
-      }
-    });
+  if (!req.body.name || !req.body.username || !req.body.email
+  || !req.body.password) {
+    return res.status(400).json({ success: false, msg: 'Please no input field can be empty!' });
   }
+  const newUser = User({
+    name: req.body.name,
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  });
+
+  newUser.save((err, user) => {
+    if (err) {
+      if (err.errmsg.includes('email')) {
+        res.status(400).json({ success: false, msg: 'email already exist' });
+      } else if (err.errmsg.includes('username')) {
+        res.status(400).json({ success: false, msg: 'user already exist' });
+      } else {
+        res.status(400).json(err);
+      }
+    } else {
+      const token = jwt.sign({
+        id: user.id,
+        exp: moment().add(7, 'd').valueOf()
+      }, 'secret');
+      res.status(201).json({
+        success: true, msg: 'You have successfully signed up!', token
+      });
+    }
+  });
 };
