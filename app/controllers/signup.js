b51ef20610d8e8dabf580dcee74c0ecf44f1ea6e
andela-uenyingwa <uloaku.enyingwa@andela.com>
2017-02-01 12:41:56 +0100

    feature(authentication): modify JS file

--- app/controllers/signup.js
+++ app/controllers/signup.js
@@ -5,45 +5,42 @@ const moment = require('moment');
 const User = mongoose.model('User');
 
 exports.signup = (req, res) => {
-  if (!req.body.name || !req.body.username || !req.body.email ||
-    !req.body.password) {
-    return res.status(400).json({
-      success: false,
-      msg: 'Please no input field can be empty!'
-    });
-  }
   const newUser = User({
     name: req.body.name,
     username: req.body.username,
     email: req.body.email,
     password: req.body.password
   });
-
-  newUser.save((err, user) => {
-    if (err) {
-      if (err.errmsg.includes('email')) {
-        res.status(400).json({
-          success: false,
-          msg: 'email already exist'
-        });
-      } else if (err.errmsg.includes('username')) {
-        res.status(400).json({
-          success: false,
-          msg: 'user already exist'
-        });
+  if (!req.body.name || !req.body.username || !req.body.email ||
+    !req.body.password) {
+    res.status(400).json({
+      msg: 'Please no input field can be empty!'
+    });
+  } else {
+    newUser.save((err, user) => {
+      if (err) {
+        if (err.errmsg.includes('email')) {
+          res.status(400).json({
+            msg: 'email already exist, please use another email'
+          });
+        } else if (err.errmsg.includes('username')) {
+          res.status(400).json({
+            msg: 'user already exist, please use another username'
+          });
+        } else {
+          res.status(400).json(err);
+        }
       } else {
-        res.status(400).json(err);
+        const token = jwt.sign({
+          id: user.id,
+          exp: moment().add(7, 'd').valueOf()
+        }, 'secret');
+        res.status(201).json({
+          msg: 'You have successfully signed up!',
+          token
+        });
       }
-    } else {
-      const token = jwt.sign({
-        id: user.id,
-        exp: moment().add(7, 'd').valueOf()
-      }, 'secret');
-      res.status(201).json({
-        success: true,
-        msg: 'You have successfully signed up!',
-        token
-      });
-    }
-  });
+    });
+  }
 };
+
