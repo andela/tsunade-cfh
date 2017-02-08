1f15b2856ef58cb76127c7386e56f1b89f80adf0
Seyi-Keye <oluwaseyi.aromokeye@andela.com>
2017-02-08 17:51:17 +0100

    (start-game): modal to gaming screen

---
+++ public/js/modal.js
@@ -0,0 +1,19 @@
+// Get the modal
+const modal = document.getElementById('myModal');
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName('close')[0];
+
+// Open modal as soon as page opens
+modal.style.display = 'block';
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = () => {
+  modal.style.display = 'none';
+};
+
+const start = document.getElementById('start-game');
+
+start.onclick = () => {
+  $scope.startGame = game.startGame();
+};
