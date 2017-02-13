ef8b0b7d1d5250f9a82c361510d8504e89d0d854
andela-osubair <oyindamola.subair@andela.com>
2017-02-13 09:17:42 +0100

    feature(redesign-auth-screen): fix hound error

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,5 +1,5 @@
 angular.module('mean.system')
-.controller('IndexController', ['$scope', 'Global', '$location', 'socket', 'game', 'AvatarService', '$http', function ($scope, Global, $location, socket, game, AvatarService, $http) {
+  .controller('IndexController', ['$scope', 'Global', '$location', 'socket', 'game', 'AvatarService', '$http', function($scope, Global, $location, socket, game, AvatarService, $http) {
     $scope.global = Global;
 
     $scope.playAsGuest = () => {
@@ -17,118 +17,122 @@ angular.module('mean.system')
 
     $scope.avatars = [];
     AvatarService.getAvatars()
-      .then(function(data) {
+      .then((data) => {
         $scope.avatars = data;
       });
 
     $scope.login = () => {
-       var user = {
-         email: $scope.login_email,
-         password: $scope.login_password
-       };
+      const user = {
+        email: $scope.login_email,
+        password: $scope.login_password
+      };
 
-       $http.post('/api/auth/login', user).then(function() {
-         $location.path('/app');
-       }, (err) => {
-         $scope.showError();
-         $scope.error = err;
-       });
-    };
-
-    $scope.signup = () => {
-      if(!$scope.name || !$scope.email || !$scope.password) {
-         const error = {
-           data: { message: 'Data incomplete.' }
-         };
-         $scope.showSignUpErrors(error);
-       } else {
-         var newuser = {
-           email: $scope.email,
-           username: $scope.username,
-           password: $scope.password,
-           name: $scope.name
-         };
-
-         $http.post('/api/auth/signup', newuser).then(function() {
-           $location.path('/app');
-         }, (err) => {
-           $scope.showError();
-           $scope.error = err;
-         });
-       }
-   };
-
-    $scope.facebookLogin = () => {
-       const facebook = hello('facebook');
-       facebook.login({ scope: 'email' }).then(
-         () => {
-           hello('facebook').api('me').then(function(user) {
-             const userDetails = {
-               name: user.name,
-               email: user.email,
-               provider: 'facebook'
-             };
-             $http.post('/api/auth/social', userDetails).then(function() {
-               $location.path('/app');
-             });
-           });
-         },
-         (err) => {
-           $scope.showError();
-           $scope.error = err;
-         }
-       );
-   };
-
-   $scope.twitterLogin = () => {
-       const twitter = hello('twitter');
-       twitter.login({ scope: 'email' }).then(
-       () => {
-         hello('twitter').api('me').then(function(user) {
-           const userDetails = {
-             name: user.name,
-             email: user.id,
-             provider: 'twitter'
-           };
-           $http.post('/api/auth/social', userDetails).then(function() {
-             $location.path('/app');
-           });
-         });
-       }, (err) => {
-         $scope.showError();
-         $scope.error = err;
+      $http.post('/api/auth/login', user).then(() => {
+        $location.path('/app');
+      }, (err) => {
+        $scope.showError();
+        $scope.error = err;
       });
     };
 
-   $scope.googleLogin = () => {
-     const google = hello('google');
-     google.login({ redirect_uri: 'http://localhost:3000/', scope: 'email'}).then(
-       () => {
-         hello('google').api('me').then(function(user) {
-           const userDetails = {
-             name: user.name,
-             email: user.email,
-             provider: 'google'
-           };
-           $http.post('/api/auth/social', userDetails).then(function() {
-             $location.path('/app');
-           });
-         });
-       }, (err) => {
-         $scope.showError();
-         $scope.error = err;
-     });
-   };
+    $scope.signup = () => {
+      if (!$scope.name || !$scope.username || !$scope.email || !$scope.password) {
+        const error = {
+          data: { message: 'Data incomplete.' }
+        };
+        $scope.showError();
+        $scope.error = err;
+      } else {
+        const newuser = {
+          email: $scope.email,
+          username: $scope.username,
+          password: $scope.password,
+          name: $scope.name
+        };
 
-   $scope.start = () => {
-     hello.init({
-       facebook: "1867221413501027",
-       google: "825562793958-hsbrf2tri06upbvs40tbhept97a5t5qs.apps.googleusercontent.com",
-       twitter: "EsqPppinJViefxGH67ILoM7AJ"
-     }, {
-       redirect_uri: '/'
-     })
-   };
+        $http.post('/api/auth/signup', newuser).then(() => {
+          $location.path('/app');
+        }, (err) => {
+          $scope.showError();
+          $scope.error = err;
+        });
+      }
+    };
 
-   $scope.start();
-}]);
+    $scope.facebookLogin = () => {
+      const facebook = hello('facebook');
+      facebook.login({ scope: 'email' }).then(
+        () => {
+          hello('facebook').api('me').then((user) => {
+            const userDetails = {
+              name: user.name,
+              email: user.email,
+              provider: 'facebook'
+            };
+            $http.post('/api/auth/social', userDetails).then(() => {
+              $location.path('/app');
+            });
+          });
+        },
+        (err) => {
+          $scope.showError();
+          $scope.error = err;
+        }
+      );
+    };
+
+    $scope.twitterLogin = () => {
+      const twitter = hello('twitter');
+      twitter.login({ scope: 'email' }).then(
+        () => {
+          hello('twitter').api('me').then((user) => {
+            const userDetails = {
+              name: user.name,
+              email: user.id,
+              provider: 'twitter'
+            };
+            $http.post('/api/auth/social', userDetails).then(() => {
+              $location.path('/app');
+            });
+          });
+        }, (err) => {
+          $scope.showError();
+          $scope.error = err;
+        });
+    };
+
+    $scope.googleLogin = () => {
+      const google = hello('google');
+      google.login({
+        redirect_uri: 'http://localhost:3000/',
+        scope: 'email'
+      }).then(
+        () => {
+          hello('google').api('me').then((user) => {
+            const userDetails = {
+              name: user.name,
+              email: user.email,
+              provider: 'google'
+            };
+            $http.post('/api/auth/social', userDetails).then(() => {
+              $location.path('/app');
+            });
+          });
+        }, (err) => {
+          $scope.showError();
+          $scope.error = err;
+        });
+    };
+
+    $scope.start = () => {
+      hello.init({
+        facebook: '1867221413501027',
+        google: '825562793958-hsbrf2tri06upbvs40tbhept97a5t5qs.apps.googleusercontent.com',
+        twitter: 'EsqPppinJViefxGH67ILoM7AJ'
+      }, {
+        redirect_uri: '/'
+      });
+    };
+
+    $scope.start();
+  }]);
