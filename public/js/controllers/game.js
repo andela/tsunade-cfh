0bad8668a0d57c52cfa06b1cfddc7b826bfba085
andela-uofoegbu <ugonna.ofoegbu@andela.com>
2017-02-14 12:14:28 +0100

    Include chat app

--- public/js/controllers/game.js
+++ public/js/controllers/game.js
@@ -22,107 +22,91 @@ angular.module('mean.system')
             $scope.hasPickedCards = true;
           } else if (game.curQuestion.numAnswers === 2 &&
             $scope.pickedCards.length === 2) {
-            //delay and send
-            $scope.hasPickedCards = true;
-            $timeout($scope.sendPickedCards, 300);
-          }
-        } else {
-          $scope.pickedCards.pop();
+            // delay and send
+          $scope.hasPickedCards = true;
+          $timeout($scope.sendPickedCards, 300);
         }
-      }
-    };
-
-    $scope.pointerCursorStyle = function() {
-      if ($scope.isCzar() && $scope.game.state === 'waiting for czar to decide') {
-        return {'cursor': 'pointer'};
       } else {
-        return {};
+        $scope.pickedCards.pop();
       }
-    };
+    }
+  };
 
-    $scope.sendPickedCards = function() {
-      game.pickCards($scope.pickedCards);
-      $scope.showTable = true;
-    };
+  $scope.pointerCursorStyle = () => {
+    if ($scope.isCzar() && $scope.game.state === 'waiting for czar to decide') {
+      return { cursor: 'pointer' };
+    }
+    return {};
+  };
 
-    $scope.cardIsFirstSelected = function(card) {
-      if (game.curQuestion.numAnswers > 1) {
-        return card === $scope.pickedCards[0];
-      } else {
-        return false;
-      }
-    };
+  $scope.sendPickedCards = () => {
+    game.pickCards($scope.pickedCards);
+    $scope.showTable = true;
+  };
 
-    $scope.cardIsSecondSelected = function(card) {
-      if (game.curQuestion.numAnswers > 1) {
-        return card === $scope.pickedCards[1];
-      } else {
-        return false;
-      }
-    };
+  $scope.cardIsFirstSelected = (card) => {
+    if (game.curQuestion.numAnswers > 1) {
+      return card === $scope.pickedCards[0];
+    }
+    return false;
+  };
 
-    $scope.firstAnswer = function($index){
-      if($index % 2 === 0 && game.curQuestion.numAnswers > 1){
-        return true;
-      } else{
-        return false;
-      }
-    };
+  $scope.cardIsSecondSelected = (card) => {
+    if (game.curQuestion.numAnswers > 1) {
+      return card === $scope.pickedCards[1];
+    }
+    return false;
+  };
 
-    $scope.secondAnswer = function($index){
-      if($index % 2 === 1 && game.curQuestion.numAnswers > 1){
-        return true;
-      } else{
-        return false;
-      }
-    };
+  $scope.firstAnswer = ($index) => {
+    if ($index % 2 === 0 && game.curQuestion.numAnswers > 1) {
+      return true;
+    }
+    return false;
+  };
 
-    $scope.showFirst = function(card) {
-      return game.curQuestion.numAnswers > 1 && $scope.pickedCards[0] === card.id;
-    };
+  $scope.secondAnswer = ($index) => {
+    if ($index % 2 === 1 && game.curQuestion.numAnswers > 1) {
+      return true;
+    }
+    return false;
+  };
 
-    $scope.showSecond = function(card) {
-      return game.curQuestion.numAnswers > 1 && $scope.pickedCards[1] === card.id;
-    };
+  $scope.showFirst = card => game.curQuestion.numAnswers > 1 && $scope.pickedCards[0] === card.id;
 
-    $scope.isCzar = function() {
-      return game.czar === game.playerIndex;
-    };
+  $scope.showSecond = card => game.curQuestion.numAnswers > 1 && $scope.pickedCards[1] === card.id;
 
-    $scope.isPlayer = function($index) {
-      return $index === game.playerIndex;
-    };
+  $scope.isCzar = () => game.czar === game.playerIndex;
 
-    $scope.isCustomGame = function() {
-      return !(/^\d+$/).test(game.gameID) && game.state === 'awaiting players';
-    };
+  $scope.isPlayer = ($index) => {
+    $window.sessionStorage
+    .setItem('chatUsername', game.players[game.playerIndex].username);
+    $window.sessionStorage
+    .setItem('avatar', game.players[game.playerIndex].avatar);
+    return $index === game.playerIndex;
+  };
 
-    $scope.isPremium = function($index) {
-      return game.players[$index].premium;
-    };
+  $scope.isCustomGame = () => !(/^\d+$/).test(game.gameID) && game.state === 'awaiting players';
 
-    $scope.currentCzar = function($index) {
-      return $index === game.czar;
-    };
+  $scope.isPremium = $index => game.players[$index].premium;
 
-    $scope.winningColor = function($index) {
-      if (game.winningCardPlayer !== -1 && $index === game.winningCard) {
-        return $scope.colors[game.players[game.winningCardPlayer].color];
-      } else {
-        return '#f9f9f9';
-      }
-    };
+  $scope.currentCzar = $index => $index === game.czar;
 
-    $scope.pickWinning = function(winningSet) {
-      if ($scope.isCzar()) {
-        game.pickWinning(winningSet.card[0]);
-        $scope.winningCardPicked = true;
-      }
-    };
+  $scope.winningColor = ($index) => {
+    if (game.winningCardPlayer !== -1 && $index === game.winningCard) {
+      return $scope.colors[game.players[game.winningCardPlayer].color];
+    }
+    return '#f9f9f9';
+  };
 
-    $scope.winnerPicked = function() {
-      return game.winningCard !== -1;
-    };
+  $scope.pickWinning = (winningSet) => {
+    if ($scope.isCzar()) {
+      game.pickWinning(winningSet.card[0]);
+      $scope.winningCardPicked = true;
+    }
+  };
+
+  $scope.winnerPicked = () => game.winningCard !== -1;
 
     $scope.startGame = function() {
       const isUptoRequiredNumber = game.players.length >= game.playerMinLimit;
@@ -130,38 +114,38 @@ angular.module('mean.system')
       ) : $('#playerMinimumAlert').modal('show');
     };
 
-    $scope.abandonGame = function() {
-      game.leaveGame();
-      $location.path('/');
-    };
+  $scope.abandonGame = () => {
+    game.leaveGame();
+    $location.path('/');
+  };
 
     // Catches changes to round to update when no players pick card
     // (because game.state remains the same)
-    $scope.$watch('game.round', function() {
-      $scope.hasPickedCards = false;
-      $scope.showTable = false;
-      $scope.winningCardPicked = false;
-      $scope.makeAWishFact = makeAWishFacts.pop();
-      if (!makeAWishFacts.length) {
-        makeAWishFacts = MakeAWishFactsService.getMakeAWishFacts();
-      }
-      $scope.pickedCards = [];
-    });
+  $scope.$watch('game.round', () => {
+    $scope.hasPickedCards = false;
+    $scope.showTable = false;
+    $scope.winningCardPicked = false;
+    $scope.makeAWishFact = makeAWishFacts.pop();
+    if (!makeAWishFacts.length) {
+      makeAWishFacts = MakeAWishFactsService.getMakeAWishFacts();
+    }
+    $scope.pickedCards = [];
+  });
 
     // In case player doesn't pick a card in time, show the table
-    $scope.$watch('game.state', function() {
-      if (game.state === 'waiting for czar to decide' && $scope.showTable === false) {
-        $scope.showTable = true;
-      }
-    });
+  $scope.$watch('game.state', () => {
+    if (game.state === 'waiting for czar to decide' && $scope.showTable === false) {
+      $scope.showTable = true;
+    }
+  });
 
-    $scope.$watch('game.gameID', function() {
-      if (game.gameID && game.state === 'awaiting players') {
-        if (!$scope.isCustomGame() && $location.search().game) {
+  $scope.$watch('game.gameID', () => {
+    if (game.gameID && game.state === 'awaiting players') {
+      if (!$scope.isCustomGame() && $location.search().game) {
           // If the player didn't successfully enter the request room,
           // reset the URL so they don't think they're in the requested room.
-          $location.search({});
-        } else if ($scope.isCustomGame() && !$location.search().game) {
+        $location.search({});
+      } else if ($scope.isCustomGame() && !$location.search().game) {
           // Once the game ID is set, update the URL if this is a game with friends,
           // where the link is meant to be shared.
           $location.search({game: game.gameID});
@@ -266,3 +250,4 @@ angular.module('mean.system')
     tour.start();
   };
 }]);
+
--- server.js
+++ server.js
@@ -2,69 +2,72 @@
  * Module dependencies.
  */
 require('dotenv').config();
-var express = require('express'),
-    fs = require('fs'),
-    passport = require('passport'),
-    logger = require('mean-logger'),
-    io = require('socket.io');
+const express = require('express'),
+  fs = require('fs'),
+  passport = require('passport'),
+  logger = require('mean-logger'),
+  io = require('socket.io');
 
 /**
  * Main application entry file.
  * Please note that the order of loading is important.
  */
 
-//Load configurations
-//if test env, load example file
-var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development',
-    config = require('./config/config'),
-    auth = require('./config/middlewares/authorization'),
-    mongoose = require('mongoose');
+// Load configurations
+// if test env, load example file
+const env = process.env.NODE_ENV = process.env.NODE_ENV || 'development',
+  config = require('./config/config'),
+  auth = require('./config/middlewares/authorization'),
+  mongoose = require('mongoose');
 
-//Bootstrap db connection
-var db = mongoose.connect(config.db);
+// Bootstrap db connection
+const db = mongoose.connect(config.db);
 
-//Bootstrap models
-var models_path = __dirname + '/app/models';
-var walk = function(path) {
-    fs.readdirSync(path).forEach(function(file) {
-        var newPath = path + '/' + file;
-        var stat = fs.statSync(newPath);
-        if (stat.isFile()) {
-            if (/(.*)\.(js|coffee)/.test(file)) {
-                require(newPath);
-            }
-        } else if (stat.isDirectory()) {
-            walk(newPath);
-        }
-    });
+// Bootstrap models
+const models_path = `${__dirname}/app/models`;
+const walk = (path) => {
+  fs.readdirSync(path).forEach((file) => {
+    const newPath = `${path}/${file}`;
+    const stat = fs.statSync(newPath);
+    if (stat.isFile()) {
+      if (/(.*)\.(js|coffee)/.test(file)) {
+        require(newPath);
+      }
+    } else if (stat.isDirectory()) {
+      walk(newPath);
+    }
+  });
 };
 walk(models_path);
 
-//bootstrap passport config
+// bootstrap passport config
 require('./config/passport')(passport);
 
-var app = express();
+const app = express();
 
-app.use(function(req, res, next){
-    next();
+app.use((req, res, next) => {
+  next();
 });
 
-//express settings
+// express settings
 require('./config/express')(app, passport, mongoose);
 
-//Bootstrap routes
+// Bootstrap routes
 require('./config/routes')(app, passport, auth);
 
-//Start the app by listening on <port>
-var port = config.port;
-var server = app.listen(port);
-var ioObj = io.listen(server, { log: false });
-//game logic handled here
-require('./config/socket/socket')(ioObj);
-console.log('Express app started on port ' + port);
+// Start the app by listening on <port>
+const port = config.port;
+const server = app.listen(port);
+const ioObj = io.listen(server, { log: false });
 
-//Initializing logger
+// game logic handled here
+require('./config/socket/socket')(ioObj);
+
+console.log(`Express app started on port ${port}`);
+
+// Initializing logger
 logger.init(app, passport, mongoose);
 
-//expose app
+
+// expose app
 exports = module.exports = app;
